e624923453cc1c2bcf9322988d2c3e547f683c63
SHUBH <ealders1@asu.edu>
2022-05-17 19:55:49 +0530

    Day 8 Completed

---
+++ lib/models/catalog.dart
@@ -0,0 +1,28 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item(
+      {required this.id,
+      required this.name,
+      required this.desc,
+      required this.price,
+      required this.color,
+      required this.image});
+}
+
+final products = [
+  Item(
+    id: "Product NO.1",
+    name: "iPhone 12 Pro",
+    desc: "Apple iPhone 12th Generation",
+    price: 999,
+    color: "#33505a",
+    image:
+        "https://maalgadi.in/wp-content/uploads/2021/06/apple_iphone-12_2-up_geo_10132020_inline.jpg.large_.jpg",
+  )
+];
