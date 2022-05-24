ea278b89d256ef222b14392e9e68a43276b0b01a
SHUBH <ealders1@asu.edu>
2022-05-24 19:23:27 +0530

    Day 19 Completed

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -37,7 +37,10 @@ class _CartTotal extends StatelessWidget {
           "\$9999".text.xl5.make(),
           30.widthBox,
           ElevatedButton(
-            onPressed: () {},
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                  (SnackBar(content: "Buying not supported yet".text.make())));
+            },
             style: ButtonStyle(
                 backgroundColor:
                     MaterialStateProperty.all(MyTheme.lightBluishColor)),
@@ -65,7 +68,9 @@ class _CartListState extends State<_CartList> {
         leading: Icon(Icons.done),
         trailing: IconButton(
           onPressed: () {},
-          icon: Icon(Icons.remove),
+          icon: Icon(
+            Icons.remove,
+          ),
         ),
         title: "Item 1".text.make(),
       ),
